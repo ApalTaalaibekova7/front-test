b19e5ada9823c2d2f2e268a7c9f371fc42c687b8
ApalTaalaibekova7 <apaltaalaibekova7@gmail.com>
2025-02-04 08:13:41 +0300

    add info sections

---
+++ script.js
@@ -0,0 +1,13 @@
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const swiper = new Swiper('.mySwiper', {
+      navigation: true,
+      pagination: { clickable: true },
+      keyboard: true,
+      mousewheel: true,
+      cssMode: true,
+    });
+  });
+
+
+
